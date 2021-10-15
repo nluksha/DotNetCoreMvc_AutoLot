0a4bb4d45dfb496c83fbb132f2a7b0aa6ac90926
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-15 21:12:49 +0300

    Add Tag helper

---
+++ DotNetCoreMvc_AutoLot/DotNetCoreMvc_AutoLot/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DotNetCoreMvc_AutoLot.TagHelpers
+{
+    public class EmailTagHelper: TagHelper
+    {
+        public string EmailName { get; set; }
+        public string EmailDomain { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var address = EmailName + "@" + EmailDomain;
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", "mailto:" + address);
+            output.Content.SetContent(address);
+        }
+    }
+}
