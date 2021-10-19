5c44d7928712894d4f1de189dfaca69690c26fab
Nikita Luksha <nikita.luksha@softensity.com>
2021-10-19 09:44:51 +0300

    And Create, Edit Views

--- DotNetCoreMvc_AutoLot/DotNetCore_AutoLotDAL.Models/Inventory.cs
+++ DotNetCoreMvc_AutoLot/DotNetCore_AutoLotDAL.Models/Inventory.cs
@@ -10,13 +10,13 @@ namespace DotNetCore_AutoLotDAL.Models
     [Table("Inventory")]
     public partial class Inventory : EntityBase
     {
-        [StringLength(50)]
+        [Required, StringLength(50)]
         public string Make { get; set; }
 
-        [StringLength(50)]
+        [Required, StringLength(50)]
         public string Color { get; set; }
 
-        [StringLength(50)]
+        [Required, StringLength(50)]
         public string PetName { get; set; }
 
         public List<Order> Orders { get; set; } = new List<Order>();
